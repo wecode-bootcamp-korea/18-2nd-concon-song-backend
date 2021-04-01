59c6fe23d23bd31595750e6804e7d3b4110d17b0
이수진 <s2jiin@isujin-ui-MacBookPro.local>
2021-04-01 13:43:49 +0900

    Update : product app foreignkey 경로 수정

--- product/models.py
+++ product/models.py
@@ -10,13 +10,12 @@ class Collection(models.Model):
         db_table = 'collections'
 
 class Product(models.Model):
-    collection       = models.ForeignKey('product.Collection', on_delete=models.CASCADE)
+    collection       = models.ForeignKey(Collection, on_delete=models.CASCADE)
     name             = models.CharField(max_length=100)
     price            = models.DecimalField(max_digits=18, decimal_places=0)
     created_at       = models.DateTimeField(auto_now_add=True)
     updated_at       = models.DateTimeField(auto_now=True)
     stock            = models.IntegerField(default=10)
-    main_image       = models.URLField(max_length=500)
     item_code        = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     color            = models.CharField(max_length=100)
 
@@ -33,25 +32,32 @@ class Color(models.Model):
 
 class Size(models.Model):
     name    = models.CharField(max_length=100)
-    product = models.ManyToManyField('product.Product', through='ProductSize')
+    product = models.ManyToManyField(Product, through='ProductSize')
 
     class Meta:
         db_table = 'sizes'
 
 
 class ProductSize(models.Model):
-    size    = models.ForeignKey('product.Size', on_delete=models.CASCADE)
-    product = models.ForeignKey('product.Product', on_delete=models.CASCADE)
+    size    = models.ForeignKey(Size, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'product_sizes'
 
 
 class ProductDetail(models.Model):
-    product      = models.ForeignKey('product.Product', on_delete=models.CASCADE)
+    product      = models.ForeignKey(Product, on_delete=models.CASCADE)
     description  = models.TextField()
     introduction = models.CharField(max_length=100)
-    url          = models.URLField(max_length=500)
 
     class Meta:
         db_table = 'product_detail'
+
+
+class Image(models.Model):
+    url       = models.URLField(max_length=500)
+    product   = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'product_images'
