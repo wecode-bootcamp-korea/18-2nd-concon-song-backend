785ac355d0b18d991047ebf585d963924b847b7d
jadyJeong <coldfeet0816@gmail.com>
2021-04-06 10:12:43 +0900

    CREATE: account views+login decorator+unittest created.

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+import unittest
+import requests,jwt
 
-# Create your tests here.
+from unittest.mock  import MagicMock,patch
+
+from django.test    import TestCase, Client
+
+from .models        import Account
+
+
+class AccountTest(TestCase):
+
+#-----------fixture setting
+    def setUp(self):
+        client = Client()
+        account_testing = Account(
+            email    = 'nicky@gmail.com',
+            nickname = 'nicky',
+            kakao_id = '123456'
+        )
+        account_testing.save()
+
+    def tearDown(self):
+        Account.objects.all().delete()
+
+#----------Test for sign-in
+    @patch('account.views.requests')
+    def test_get_signin(self,mock_request):
+        client = Client()
+
+        class MockResponse:
+            def json(self):
+                return  {
+                    'kakao_account':{'email':'nicky@gmail.com'},
+                    'properties' : {'nickname':'nicky'},
+                    'id' : '123456'
+                }
+
+        mock_request.get = MagicMock(return_value=MockResponse())
+        header = {'Authorization': 'access_token'}
+        response = client.get(
+            '/account/social-login',
+            ContentType ='application/json',
+            **header
+            )
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_not_found(self):
+        client = Client()
+        header = {'Authorization': 'access_token'}
+        response = client.get(
+            '/wrongwrong',
+            ContentType ='application/json',
+            **header
+            )
+        self.assertEqual(response.status_code, 404)
--- concon_song/urls.py
+++ concon_song/urls.py
@@ -17,4 +17,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('product', include('product.urls')),
+    path('account',include('account.urls'))
 ]
